303bf3ea6dcc37bded987595356bf8318bbca0a3
Krisez <740879437@qq.com>
2018-01-25 16:59:58 +0800

    init

--- app/src/main/java/cn/krisez/sign/ui/MainActivity.java
+++ app/src/main/java/cn/krisez/sign/ui/MainActivity.java
@@ -1,9 +1,9 @@
-package cn.krisez.sign;
+package cn.krisez.sign.ui;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -12,25 +12,59 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.GridLayout;
+import android.widget.ProgressBar;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import cn.krisez.sign.R;
+import cn.krisez.sign.adapter.ClassAdapter;
+import cn.krisez.sign.bean.Seats;
+import cn.krisez.sign.persenter.ClassPresenter;
+import cn.krisez.sign.persenter.ClassPresenterImp;
 
 public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+        implements NavigationView.OnNavigationItemSelectedListener,IMainView {
+
+    private RecyclerView mRecyclerView;
+    private ClassAdapter mClassAdapter;
+    private List<Seats> mSeatsLis = new ArrayList<>();
+
+    //demo
+    private Button mButton;
+    private ProgressBar mProgressBar;
+
+    private ClassPresenterImp mPresenter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mPresenter = new ClassPresenter(this);
+        init();
+        initOperation();
+    }
+
+    //操作初始化
+    private void initOperation() {
+        mClassAdapter = new ClassAdapter(this,mSeatsLis);
+        mRecyclerView = findViewById(R.id.class_recycler);
+        mRecyclerView.setLayoutManager(new GridLayoutManager(this,13, GridLayout.VERTICAL,false));
+        mRecyclerView.setItemAnimator(new DefaultItemAnimator());
+        mRecyclerView.setAdapter(mClassAdapter);
+    }
+
+    //系统初始化
+    private void init() {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        mButton = findViewById(R.id.button);
+        mProgressBar = findViewById(R.id.progress_bar);
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -97,5 +131,21 @@ public class MainActivity extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    @Override
+    public void showProgress() {
+        mProgressBar.setVisibility(View.VISIBLE);
+    }
+
+    @Override
+    public void dismissProgress() {
+        mProgressBar.setVisibility(View.GONE);
+    }
+
+    @Override
+    public void updateList(List<Seats> seats) {
+        mSeatsLis.addAll(seats);
+        mClassAdapter.notifyDataSetChanged();
     }
 }
